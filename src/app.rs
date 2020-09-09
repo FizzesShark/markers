e8412746e51e0dbbb7ca95834f16477cc5da4a92
Gary Xin <gary.xin19@gmail.com>
2020-09-08 21:54:16 -0700

    new class function

--- src/app.rs
+++ src/app.rs
@@ -3,10 +3,11 @@ use rocket::request::{self, Form, FromRequest, Request};
 use rocket::response::Redirect;
 use rocket::Outcome;
 use rocket_contrib::templates::Template;
+use rocket_contrib::json::Json;
 
 use std::collections::HashMap;
 
-use crate::db::{self, AccountError, LoginError, User};
+use crate::db::*;
 
 #[derive(FromForm)]
 struct LoginAttempt {
@@ -25,9 +26,9 @@ fn login(mut cookies: Cookies, login_attempt: Form<LoginAttempt>) -> Redirect {
     let user = &login_attempt.email;
     let pass = &login_attempt.password;
 
-    match db::validate_user(user, pass) {
+    match validate_user(user, pass) {
         Ok(()) => {
-            cookies.add(Cookie::new("id", db::generate_session(user)));
+            cookies.add(Cookie::new("id", generate_session(user)));
             Redirect::to(uri!(default))
         }
         Err(LoginError::NoSuchEmail) => Redirect::to(uri!(main: "No such email".to_string())),
@@ -45,7 +46,7 @@ struct NewUser {
 
 #[post("/register", data = "<new_user>")]
 fn register_new_user(new_user: Form<NewUser>) -> &'static str {
-    match db::add_user(&new_user.email, &new_user.password, &new_user.user_type) {
+    match add_user(&new_user.email, &new_user.password, &new_user.user_type) {
         Ok(_) => "Account successfully registered!",
         Err(AccountError::DuplicateEmail) => "An account with that email already exists.",
     }
@@ -55,7 +56,7 @@ impl<'a, 'r> FromRequest<'a, 'r> for User {
     type Error = ();
 
     fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
-        match db::validate_login(request.cookies().get("id").unwrap().value()) {
+        match validate_login(request.cookies().get("id").unwrap().value()) {
             Ok(user) => Outcome::Success(user),
             Err(()) => Outcome::Forward(()),
         }
@@ -76,15 +77,25 @@ fn default_redirect() -> Redirect {
 }
 
 #[put("/new_class?<class_type>&<class_name>")]
-fn new_class(class_type: String, class_name: String) {
-	
+fn new_class(user: User, class_type: String, class_name: String) -> Result<Json<Class>, Status> {
+	let c_type = match class_type.as_str() {
+		"open" => ClassType::Open,
+		"inviteonly" => ClassType::InviteOnly,
+		"closed" => ClassType::Closed,
+		&_ => ClassType::Open,
+	};
+
+	match create_class(user, &class_name, c_type) {
+		Ok(class) => Ok(Json(class)),
+		Err(()) => Err(Status::BadRequest)
+	}
 }
 
 pub fn start_server() -> rocket::Rocket {
     rocket::ignite()
         .mount(
             "/",
-            rocket::routes![login, main, default, default_redirect, register_new_user],
+            rocket::routes![login, main, default, default_redirect, register_new_user, new_class],
         )
         .attach(Template::fairing())
 }
